2cdff2d13c2b4b6c96a8ffc771c5b2bddebff4ba
nithin88-ops <58203293+nithin88-ops@users.noreply.github.com>
2021-10-07 14:24:56 +0530

    updated

--- config/db.js
+++ config/db.js
@@ -1,19 +1,19 @@
-// const mongoose = require('mongoose');
-// const config = require('config');
-// const { compareSync } = require('bcryptjs');
-// const db = config.get('mongoURI');
+const mongoose = require('mongoose');
+const config = require('config');
 
-// const connectDB = async() => {
-//     try{
-//         await mongoose.connect(db, {
-//             useNewUrlParser: true
-//         });
-//         console.log("MongoDB Connected");
-//     }
-//     catch(err) {
-//         console.error(err.message);
-//         process.exit(1);
-//     }
-// }
+const db = config.get('mongoURI');
 
-// module.exports = connectDB;
+const connectDB = async() => {
+    try{
+        await mongoose.connect(db, {
+            useNewUrlParser: true
+        });
+        console.log("MongoDB Connected");
+    }
+    catch(err) {
+        console.error(err.message);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
